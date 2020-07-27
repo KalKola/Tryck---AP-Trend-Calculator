dd921c4bcc0a2869a53dc82dc56784481b490f4b
HiddenDongo <kalyankolacahill@gmail.com>
2020-07-27 10:35:38 +0200

    Data Format Class

--- src/DataFormatter.java
+++ src/DataFormatter.java
@@ -1,2 +1,17 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DataFormatter {
+    Date date;
+    float pressure;
+    float humidity;
+
+    public String toString() {
+        DateFormat dF = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+        return ("Date: " + dF.format(date)
+                + " Barometric Pressure: " + Float.toString(pressure)
+                + " Humidity: " + Float.toString(humidity));
+    }
 }
--- src/PressureSlope.java
+++ src/PressureSlope.java
@@ -1,6 +1,14 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 public class PressureSlope {
 
+    ArrayList<DataFormatter> collectedData = new ArrayList<>();
+
     public void readData(String fileName) {
+        DateFormat dF = new SimpleDateFormat("yyyy_MM_dd HH:mm:ss");
+
 
     }
 
